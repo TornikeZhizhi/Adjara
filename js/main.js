2cd703bbefd072b052b215e0f7b4cb1c50195718
GVANTSAKHURTSID\Gvantsa <tornike.zhizhiashvili@geolab.edu.ge>
2018-11-07 18:40:46 +0400

    no message

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 
 
-
+//  Scroll Spy
 $('.scrollspy_button a').click(function(e) {
     var targetHref = $(this).attr('href');
     $('html, body').animate({
@@ -20,6 +20,110 @@ $('.scrollspy_button a').click(function(e) {
     }, 800);
     e.preventDefault();
   });
+
+
+
+
+// Hover Z index
+	
+// $(".post_fluid_content").on("mouseenter",function(){
+
+// 	$(".post_fluid_content").removeClass("active");
+// 	$(".post_fluid_content").addClass("deactive");
+// 	$(this).addClass("active");
+// })
+
+//
+
+
+
+var vids = $(".home_video_box video"); 
+$.each(vids, function(){
+       this.controls = false; 
+}); 
+
+
+$(".home_video_box video").mouseenter(function() {
+
+	
+ 
+  if (this.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+});
+
+$(".home_video_box video").mouseleave(function() {
+
+  if (this.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+});
+
+
+$(".home_video_box").mouseenter(function() {
+
+	$(this).find(".video_decor").addClass("active");
+	
+})
+
+$(".home_video_box").mouseleave(function() {
+	$(this).find(".video_decor").removeClass("active");
+})
+
+
+
+// modal video
+
+var vids = $(".home_video_modal video"); 
+$.each(vids, function(){
+       this.controls = false; 
+}); 
+//Loop though all Video tags and set Controls as false
+
+$(".home_video_modal video").click(function() {
+
+	
+  
+  if (this.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+});
+
+
+
+
+
+
+// Top Scroll
+
+$('.top_scoll a').click(function(e) {
+    var targetHref = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(targetHref).offset().top
+    }, 800);
+    e.preventDefault();
+  });
+
+
+$(window).on("scroll",function(){
+	var sm = $(this).scrollTop();
+	if (sm > 400) {
+		$(".top_scoll").fadeIn();
+	}else {
+		$(".top_scoll").fadeOut();
+	}
+	
+
+console.log(sm)
+
+})
+
 
 
 })
